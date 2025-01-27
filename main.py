eae6add02aac96657ab96cfedfcd9c4d135d3ed6
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-01-27 13:22:42 +0200

    Added the functionality of resizing the display window

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
-
+from pygame.constants import HWSURFACE, DOUBLEBUF, RESIZABLE
+from pygame.surface import Surface
 pygame.init()  # Initialize pygame
 
-# Load image to create a window
+# Create a resizable window of size 1920 x 1080, with faster rendering on screen
+window = pygame.display.set_mode((1024, 768), HWSURFACE | DOUBLEBUF | RESIZABLE)
+
+# Set size of game scene to be that of our image
 image = pygame.image.load("image.png")
-window = pygame.display.set_mode(image.get_size())
+renderWidth , renderHeight = image.get_size()
 
 running = True
 
@@ -20,7 +24,27 @@ while running:
                 running = False
                 break
     # Rendering
-    window.blit(image, (0, 0))
+    # Render our scene in a surface of the size we chose previously
+    renderSurface = Surface((renderWidth, renderHeight))
+    renderSurface.blit(image, (0, 0))
+
+    # Scale rendering when resizing window
+    windowWidth, windowHeight = window.get_size()
+    renderRatio = renderWidth / renderHeight # Compute aspect ratios of window and scene surfaces
+    windowRatio = windowWidth / windowHeight
+
+    if windowRatio <= renderRatio: # We can use full window width but not height
+        rescaledSurfaceWidth, rescaledSurfaceHeight = windowWidth, (windowWidth // renderRatio)
+        rescaledSurfaceX = 0  # Compute coordinates of rescaled surface
+        rescaledSurfaceY = (windowHeight - rescaledSurfaceHeight) // 2
+    else: # We can use full window height but not width
+        rescaledSurfaceWidth, rescaledSurfaceHeight = int (windowHeight * renderRatio), windowHeight
+        rescaledSurfaceX = (windowWidth - rescaledSurfaceWidth) // 2 # Compute coordinates again
+        rescaledSurfaceY = 0
+
+    # Scale the rendering to the window/screen size
+    rescaledSurface = pygame.transform.scale(renderSurface, (rescaledSurfaceWidth, rescaledSurfaceHeight))
+    window.blit(rescaledSurface, (rescaledSurfaceX, rescaledSurfaceY))
     pygame.display.update()
 
 pygame.quit()
