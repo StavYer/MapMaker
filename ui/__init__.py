58ef5aea129806d9ee2b6e1a3cc90436d0ebd422
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-02-24 15:14:58 +0200

    implemented abstract GameMode.py class, that all game modes must implement.

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,3 +1,4 @@
 from .UserInterface import UserInterface
 from .MouseButtons import MouseButtons
 from .MouseWheel import MouseWheel
+from .Theme import Theme
---
+++ ui/mode/GameMode.py
@@ -0,0 +1,54 @@
+from abc import abc, abstractmethod
+
+from pygame.surface import Surface
+
+from ui.MouseButtons import MouseButtons
+from ui.MouseWheel import MouseWheel
+from ui.Theme import Theme
+
+class GameMode(ABC):
+
+    def __init__(self, i_theme: Theme)
+        self.__theme = i_theme
+
+    @property
+    def theme(self) -> Theme:
+        return self.__theme
+    
+    @abstractmethod
+    def processInput(self):
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def update(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def render(self, i_surface: Surface):
+        raise NotImplementedError()
+
+    # Mouse handling
+
+    # When mouse enters render area
+    def mouseEnter(self, i_mouseX: int, i_mouseY: int, i_buttons: MouseButtons):
+        return False
+
+    # When mouse leaves render area
+    def mouseLeave(self):
+        return False
+
+    # When mouse button is pressed
+    def mouseButtonDown(self, i_mouseX: int, i_mouseY: int, i_buttons: MouseButtons):
+        return False
+
+    # When mouse button is released
+    def mouseButtonUp(self, i_mouseX: int, i_mouseY: int, i_buttons: MouseButtons):
+        return False
+
+    # When mouse wheel is scrolled
+    def mouseWheel(self, i_mouseX: int, i_mouseY: int, i_buttons: MouseButtons, i_wheel: MouseWheel):
+        return False
+
+    # When mouse moves
+    def mouseMove(self, i_mouseX: int, i_mouseY: int, i_buttons: MouseButtons):
+        return False
