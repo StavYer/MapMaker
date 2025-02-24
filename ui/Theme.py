cb806a3e5c8acc54d9ca95a506d8c599f62b49c1
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-02-24 14:16:51 +0200

    implemented Theme.py, the class for organizing data for UserInterface.py

---
+++ ui/Theme.py
@@ -0,0 +1,39 @@
+"""
+The Theme class. Contains all data related to the user-interface,
+like the size of tiles or the used tilesets.
+"""
+
+from typing import Dict, Tuple
+
+import pygame
+from pygame.surface import Surface
+
+from state.constants import LAYER_GROUND_EARTH, LAYER_GROUND_SEA
+
+class Theme:
+
+    def __init__(self):
+        # Load tileset, define tile size, and define location of tiles in tileset
+        self.__tileset = pygame.image.load("assets/toen/ground.png")
+        self.__tileWidth = 16
+        self.__tileHeight = 16
+        self.__tiles = {
+            LAYER_GROUND_EARTH: (2, 7),
+            LAYER_GROUND_SEA: (5, 7),
+        }
+    
+    @property
+    def tileWidth(self) -> int:
+        return self.__tileWidth
+    
+    @property
+    def tileHeight(self) -> int:
+        return self.__tileHeight
+
+    @property
+    def tileset(self) -> Surface:
+        return self.__tileset
+
+    @property
+    def tiles(self) -> Dict[int, Tuple[int, int]]:
+        return self.__tiles
