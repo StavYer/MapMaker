3113edac4d942bcb0d0645e5b6789f903b360702
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-02-25 15:11:04 +0200

    refactored World.py to make use of the Layer class

--- core/state/World.py
+++ core/state/World.py
@@ -1,37 +1,52 @@
+from .Layer import Layer
+from ..constants import CellValue
 class World:
     def __init__(self, input_width : int, input_height : int):
         # size of our world. Should be immutable.
         self.__width = input_width
         self.__height = input_height
-        # Value of every cell of the world. Should be immutable.
-        # A 2d matrix - each row has width columns (initialized to 0)
-        # and we have height rows
-        self.__cells = []
-        for y in range(input_height):
-            row = [0] * input_width
-            self.__cells.append(row)
+        
+        # Initialize a dictionary of layers with default cell values
+        self.__layers = {
+            "ground": Layer(input_width, input_height, CellValue.GROUND_SEA),
+            "impassable": Layer(input_width, input_height, CellValue.NONE),
+            "objects": Layer(input_width, input_height, CellValue.NONE)
+        }
 
     # Getter properties
     @property
-    def width(self):
+    def width(self) -> int:
         return self.__width
 
     @property
-    def height(self):
+    def height(self) -> int:
         return self.__height
 
-    # Getter and Setter for a single cell's value
+    @property
+    def ground(self) -> Layer:
+        return self.__layers["ground"]
+    
+    @property
+    def impassable(self) -> Layer:
+        return self.__layers["impassable"]
+    
+    @property
+    def objects(self) -> Layer:
+        return self.__layers["objects"]
 
-    def get_cell_value(self, input_x : int, input_y : int) -> int:
-        # Assert that the coordinates are at between proper limits, otherwise notify
-        assert 0 <= input_x < self.__width, f"invalid cell x coordinate: {input_x}"
-        assert 0 <= input_y < self.__height, f"invalid cell y coordinate: {input_y}"
+    # Getter for a single layer
+    @property
+    def getLayer(self, name: str) -> Layer:
+        if name not in self.__layers:
+            raise ValueError(f"Layer {name} not found")
 
-        return self.__cells[input_y][input_x]
+        return self.__layers[name]
+    
+    # Getters for all layers' names and values
+    @property
+    def layerNames(self) -> list[str]:
+        return list(self.__layers.keys())
 
-    def set_cell_value(self, input_x : int, input_y : int, value : int) -> None:
-        # Same here
-        assert 0 <= input_x < self.__width, f"invalid cell x coordinate: {input_x}"
-        assert 0 <= input_y < self.__height, f"invalid cell y coordinate: {input_y}"
-        assert 0 <= value <= 1 and type(value) == int, f"invalid cell value: {value}"
-        self.__cells[input_y][input_x] = value
+    @property
+    def layers(self) -> list[Layer]:
+        return list(self.__layers.values())
