59db9a0310da9eec64e511b915fe45ff8eaed403
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-02-25 17:14:08 +0200

    updated __init__.py in ui package

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,3 +2,5 @@ from .UserInterface import UserInterface
 from .MouseButtons import MouseButtons
 from .MouseWheel import MouseWheel
 from .Theme import Theme
+from .LayerComponent import LayerComponent
+from .Tileset import Tileset
