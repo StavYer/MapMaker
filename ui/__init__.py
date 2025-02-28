db52114903e0c90463eb030758268780efe115d1
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-02-28 18:14:04 +0200

    changed location of Theme.py and Tileset.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,6 +1,6 @@
 from .UserInterface import UserInterface
 from .Mouse import Mouse
 from .MouseWheel import MouseWheel
-from .Theme import Theme
+from ui.theme.Theme import Theme
 from .LayerComponent import LayerComponent
 from .Tileset import Tileset
