e632f163164e442024823c93b8e25d20368cee2d
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-02-28 12:01:59 +0200

    Implemented Mouse class instead of MouseButtons

---
+++ ui/Mouse.py
@@ -0,0 +1,33 @@
+# ui/Mouse.py
+from typing import Tuple
+
+class Mouse:
+    def __init__(self, coords: Tuple[int, int], buttons: Tuple[bool, bool, bool]):
+        # Initialize the Mouse object with coordinates and button states.
+        self.coords = coords
+        self.buttons = buttons
+
+        @property
+        def x(self) -> int:
+        # Get the x-coordinate of the mouse.
+            return self.coords[0]
+
+        @property
+        def y(self) -> int:
+        # Get the y-coordinate of the mouse.
+            return self.coords[1]
+
+        @property
+        def button1(self) -> bool:
+        # Get the state of the first mouse button.
+            return self.buttons[0]
+
+        @property
+        def button2(self) -> bool:
+        # Get the state of the second mouse button.
+            return self.buttons[1]
+
+        @property
+        def button3(self) -> bool:
+        # Get the state of the third mouse button.
+            return self.buttons[2]
