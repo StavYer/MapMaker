3141dedcffb23797743a9daeaebdccabf2988ef4
stav.yermiyahu <stav.yermiyahu@post.runi.ac.il>
2025-03-01 20:38:15 +0200

    updated GameMode to extend CompositeElement

--- ui/mode/GameMode.py
+++ ui/mode/GameMode.py
@@ -2,65 +2,17 @@
 GameMode.py, an abstract class to handle the high-level logics of rendering.
 """
 
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 
 from pygame.surface import Surface
 
-from ui.Mouse import Mouse
-from ui.MouseWheel import MouseWheel
+from ui.component.CompositeComponent import CompositeComponent
 from ui.theme.Theme import Theme
 
-class GameMode(ABC):
-
-    def __init__(self, i_theme: Theme):
-        self.__theme = i_theme
-
-    @property
-    def theme(self) -> Theme:
-        return self.__theme
-    
-    @abstractmethod
-    def processInput(self):
-        raise NotImplementedError()
+class GameMode(CompositeComponent):
+    """Base class for game modes that can contain components"""
     
     @abstractmethod
     def update(self):
+        """Update game state"""
         raise NotImplementedError()
-
-    @abstractmethod
-    def render(self, i_surface: Surface):
-        raise NotImplementedError()
-
-    # Keyboard handling
-    
-    def keyDown(self, i_key: int):
-        pass
-
-    def keyUp(self, i_key: int):
-        pass
-
-    # Mouse handling
-
-    # When mouse enters render area
-    def mouseEnter(self, i_mouse: Mouse):
-        return False
-
-    # When mouse leaves render area
-    def mouseLeave(self):
-        return False
-
-    # When mouse button is pressed
-    def mouseButtonDown(self, i_mouse: Mouse):
-        return False
-
-    # When mouse button is released
-    def mouseButtonUp(self, i_mouse: Mouse):
-        return False
-
-    # When mouse wheel is scrolled
-    def mouseWheel(self, i_mouse: Mouse, i_wheel: MouseWheel):
-        return False
-
-    # When mouse moves
-    def mouseMove(self, i_mouse: Mouse):
-        return False
